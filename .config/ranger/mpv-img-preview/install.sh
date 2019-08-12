4c19c3fabb6491b4c81f621a3d11f689a67862c0
durcoke <durcor@disroot.org>
2019-08-11 23:39:08 -0400

    Submodules, mpv image preview fixing

--- .config/ranger/mpv-img-preview/install.sh
+++
@@ -1,2 +0,0 @@
-sudo cp ~/.config/ranger/mpv-img-preview/fm.py /usr/lib/python3.7/site-packages/ranger/core/fm.py
-sudo cp ~/.config/ranger/mpv-img-preview/img_display.py /usr/lib/python3.7/site-packages/ranger/ext/img_display.py
